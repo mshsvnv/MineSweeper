7ac3577c8698cc7d593e2cf3853faf1c009d9ca2
mshsvnv <mshsvnv@mail.ru>
2023-01-28 19:45:54 +0300

    finish making frames

--- main.py
+++ main.py
@@ -1,11 +1,42 @@
 import tkinter as tk
+import settings as st
+import utils as ut
 
 root = tk.Tk()            # window
 
 # window settings
 root.configure(bg = "light green")  # some features of the window
-root.geometry('500x500')  # size of the window
-root.title("My Game")     # name of the window
+root.geometry(f'{st.WIDTH}x{st.HEIGHT}')  # size of the window
+root.title("Sweeper Game")     # name of the window
 root.resizable(False, False)  # disability of resizable
 
+# frame settings
+top_frame = tk.Frame(
+    root, 
+    bg = "light yellow",
+    width = st.WIDTH,
+    height = ut.height_prct(25)
+)
+top_frame.place(x = 0, y = 0)  # frame location
+
+left_frame = tk.Frame(
+    root,
+    bg = "light blue",
+    width = ut.width_prct(25),
+    height = ut.height_prct(75)
+)
+left_frame.place(x = 0, y = ut.height_prct(25))
+
+center_frame = tk.Frame(
+    root,
+    bg = "light pink",
+    width = ut.width_prct(75),
+    height = ut.height_prct(75)
+)
+center_frame.place(
+    x = ut.width_prct(25),
+    y = ut.height_prct(25)
+)
+
+# run the window
 root.mainloop()
---
+++ utils.py
@@ -0,0 +1,7 @@
+import settings as st
+
+def height_prct(percentage):
+    return int(st.HEIGHT / 100 * percentage)
+
+def width_prct(percentage):
+    return int(st.WIDTH / 100 * percentage)
